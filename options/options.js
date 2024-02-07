374f69ae62283f43a60c10df6692e4173975c620
Crowdbotics <team@crowdbotics.com>
2024-02-07 17:00:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skill-shuffle-46084",
+  url: "https://skill-shuffle-46084.botics.co",
+  api: "https://skill-shuffle-46084.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
